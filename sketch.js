df6f198039b5898d8cd550b30874918fa0bbcff5
MAHI-MALHANI <76902982+MAHI-MALHANI@users.noreply.github.com>
2021-06-05 16:51:58 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,174 +1,236 @@
-const Engine = Matter.Engine;
-const World= Matter.World;
-const Bodies = Matter.Bodies;
-const Constraint = Matter.Constraint;
+/*Change the scope of some variables from local to global to be used anywhere in the code.
+● Write a reset function to restart the game when the reset icon is pressed.
+● Set up local environment to run Trex code on the local machine*/
+/* trex is a global variable as it is to be used in all functions in this application and 
+it is defined above all the functions*/
+var PLAY = 1;
+var END = 0;
+var gameState = PLAY;
 
-var engine, world;
+var trex, trex_running, trex_collided;
+var ground, invisibleGround, groundImage;
 
-var blueLine, greenLine, yellowLine, redLine;
-var waterballoon, waterBalloonImg;
-var slingshot;
-var boy, boyImg;
-var BackgroundImg;
-var ground;
+var cloudsGroup, cloudImage;
+var obstaclesGroup, obstacle1, obstacle2, obstacle3, obstacle4, obstacle5, obstacle6;
 
-var Score=0;
-var count=1;
-var lineNumber;
-var gameState;
+var score=0;
+var gameOverImg,restartImg
+var jumpSound , checkPointSound, dieSound
 var x=0;
 var y;
-//var launchingForce=70;
 
 function preload(){
-  backgroundImg=loadImage("backgroundImg.jpg");
-  boyImg=loadImage("boy.png");
-  waterBalloonImg=loadImage("waterballoon.png");
+  trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
+  trex_collided = loadAnimation("trex_collided.png");
+  
+  groundImage = loadImage("ground2.png");
+  
+  cloudImage = loadImage("cloud.png");
+  
+  obstacle1 = loadImage("obstacle1.png");
+  obstacle2 = loadImage("obstacle2.png");
+  obstacle3 = loadImage("obstacle3.png");
+  obstacle4 = loadImage("obstacle4.png");
+  obstacle5 = loadImage("obstacle5.png");
+  obstacle6 = loadImage("obstacle6.png");
+  
+  restartImg = loadImage("restart.png")
+  gameOverImg = loadImage("gameOver.png")
+  
+  jumpSound = loadSound("jump.mp3")
+  dieSound = loadSound("die.mp3")
+  checkPointSound = loadSound("checkPoint.mp3")
 }
 
 function setup() {
-  createCanvas(1200,600);
-  engine = Engine.create();
-  world = engine.world;
+  createCanvas(600, 200);
 
-  ground= new Ground(600, 590, 1200, 20);
-  boy=new Boy(150, 495, 150, 200);
+  var message = "This is a message";
+ console.log(message)
+  
+  trex = createSprite(50,160,20,50);
+  trex.addAnimation("running", trex_running);
+  trex.addAnimation("collided", trex_collided);
+  trex.scale = 0.5;
+  
+  ground = createSprite(200,180,400,20);
+  ground.addImage("ground",groundImage);
+  ground.x = ground.width /2;
+  ground.velocityX = -(6 + 3*score/100);
 
-  blueLine=new Line(1150, 550, 10, 100, "darkBlue");
+  gameOver = createSprite(300,100);
+  gameOver.addImage(gameOverImg);
+  
+  restart = createSprite(300,140);
+  restart.addImage(restartImg);
+  
+ 
+  gameOver.scale = 0.5;
+  restart.scale = 0.5;
+  
+  invisibleGround = createSprite(200,190,400,10);
+  invisibleGround.visible = false;
+  
+  //create Obstacle and Cloud Groups
+  obstaclesGroup = createGroup();
+  cloudsGroup = createGroup();
 
-  yellowLine=new Line(950, 550, 10,100, "yellow");
-
-  greenLine=new Line(750, 550, 10,100, "green");
-
-  redLine=new Line(550, 550, 10, 100, "red");
-
-  waterballoon= new WaterBalloon(95, 450, 35, 50);
-
-  slingshot=new SlingShot(waterballoon.body,{x:95,y:450})
-  gameState="Serve";
-
+  
+  trex.setCollider("rectangle",0,0,trex.width,trex.height);
+  trex.debug = false;
+  
+  score = 0;
+  
 }
 
 function draw() {
-  background(backgroundImg);
-  Engine.update(engine);
-
-  textSize(25);
-  fill(rgb(0, 128, 255));
-  text("Score: " + Score, 1050, 50);
-  if(gameState==="Serve"){
-    textSize(25);
-    fill(rgb(0, 128, 255));
-    text("Press the space key to get a second chance!", 350, 50);
-  }
-
-  textSize(25);
-  fill("red");
-  text("10", 560, 550);
-
-  fill("green");
-  text("20", 760, 550);
-
-  fill("yellow");
-  text("30", 960, 550);
   
-  fill("Darkblue");
-  text("40", 1160, 550);
-
-if(count===4){
-  gameState="end";
-
-  if(Score>=130 && gameState==="end"){
-    textSize(30);
-    text("YOU WIN!!😀", 550, 250);
-  }
-
-  else
-  if(Score<130 && gameState==="end"){
-    textSize(30);
-    text("YOU LOSE😔", 550, 250);
-
-  }
-}
-
-camera.position.x=displayWidth/2;
-camera.position.y=y;
-
-  ground.display();
-  blueLine.display();
-  greenLine.display();
-  yellowLine.display();
-  redLine.display();
-
-  boy.display();
-  waterballoon.display();
-  slingshot.display();
+  background(180);
+  //displaying score
+  text("Score: "+ score, 500,50);
   
-  //drawSprites();
-}
+  
+  if(gameState === PLAY){
 
-function mouseDragged()
-{
-	Matter.Body.setPosition(waterballoon.body, {x:mouseX, y:mouseY});
-  console.log("mouseDragged");
-  console.log(waterballoon.body.position.x);
-  console.log(waterballoon.body.position.y);
+    gameOver.visible = false;
+    restart.visible = false;
+    
+    camera.position.x=displayWidth/2;
+    camera.position.y=y;
 
-  if(waterballoon.body.position.x >= 1150 && waterballoon.body.position.x<=1170 && waterballoon.body.position.y>= 550 && waterballoon.body.position.y<=600){
-
-    console.log("bl");
-    lineNumber=4;
+    ground.velocityX = -(4 + 3* score/100)
+    //scoring
+    score = score + Math.round(getFrameRate()/60);
+    
+    if(score>0 && score%100 === 0){
+       checkPointSound.play() 
+    }
+    
+    if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+    
+    //jump when the space key is pressed
+    if(keyDown("space")&& trex.y >= 100) {
+        trex.velocityY = -12;
+        jumpSound.play();
+    }
+    
+    //add gravity
+    trex.velocityY = trex.velocityY + 0.8
+  
+    //spawn the clouds
+    spawnClouds();
+  
+    //spawn obstacles on the ground
+    spawnObstacles();
+    
+    if(obstaclesGroup.isTouching(trex)){
+        //trex.velocityY = -12;
+        jumpSound.play();
+        gameState = END;
+        dieSound.play()
+      
+    }
   }
+   else if (gameState === END) {
+      gameOver.visible = true;
+      restart.visible = true;
+     
+     //change the trex animation
+      trex.changeAnimation("collided", trex_collided);
 
-  if(waterballoon.body.position.x >= 950 && waterballoon.body.position.x<=970 && waterballoon.body.position.y>= 550 && waterballoon.body.position.y<=600){
+      ground.velocityX = 0;
+      trex.velocityY = 0
+      
+     
+      //set lifetime of the game objects so that they are never destroyed
+    obstaclesGroup.setLifetimeEach(-1);
+    cloudsGroup.setLifetimeEach(-1);
+     
+     obstaclesGroup.setVelocityXEach(0);
+     cloudsGroup.setVelocityXEach(0);    
+   }
+  
+ 
+  //stop trex from falling down
+  trex.collide(invisibleGround);
+  
+  /* There is an instruction in the p5.play library called mousePressedOver() which
+  detects a mouse pressed over any sprite.It accepts the sprite as an argument and
+  returns true if the mouse is pressed over the sprite and false otherwise*/
+  if(mousePressedOver(restart)) {
+      reset();
+    }
 
-    console.log("yl");
-    lineNumber=3;
+  drawSprites();
+}
+
+function reset(){
+ gameState=PLAY;
+ score=0;
+  obstaclesGroup.destroyEach();
+  cloudsGroup.destroyEach();
+  
+  gameOver.visible=false;
+  restart.visible=false;
+  trex.changeAnimation("running", trex_running);
+
+}
+
+
+function spawnObstacles(){
+ if (frameCount % 60 === 0){
+   /*obstacle is a local variable here as it is defined within spawnObstacles function
+   not being used anywhere else*/
+   var obstacle = createSprite(600,165,10,40);
+   obstacle.velocityX = -(6 + score/100);
+   
+    //generate random obstacles
+    var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1: obstacle.addImage(obstacle1);
+              break;
+      case 2: obstacle.addImage(obstacle2);
+              break;
+      case 3: obstacle.addImage(obstacle3);
+              break;
+      case 4: obstacle.addImage(obstacle4);
+              break;
+      case 5: obstacle.addImage(obstacle5);
+              break;
+      case 6: obstacle.addImage(obstacle6);
+              break;
+      default: break;
+    }
+   
+    //assign scale and lifetime to the obstacle           
+    obstacle.scale = 0.5;
+    obstacle.lifetime = 300;
+   
+   //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+ }
+}
+
+function spawnClouds() {
+  //write code here to spawn the clouds
+  if (frameCount % 60 === 0) {
+    var cloud = createSprite(600,120,40,10);
+    cloud.y = Math.round(random(80,120));
+    cloud.addImage(cloudImage);
+    cloud.scale = 0.5;
+    cloud.velocityX = -3;
+    
+     //assign lifetime to the variable
+    cloud.lifetime = 200;
+    
+    //adjust the depth
+    cloud.depth = trex.depth;
+    trex.depth = trex.depth + 1;
+    
+    //add each cloud to the group
+    cloudsGroup.add(cloud);
   }
-
-  if(waterballoon.body.position.x >= 750 && waterballoon.body.position.x<=770 && waterballoon.body.position.y>= 550 && waterballoon.body.position.y<=600){
-
-    console.log("gl");
-    lineNumber=2
-  }
-
-  if(waterballoon.body.position.x >= 550 && waterballoon.body.position.x<=570 && waterballoon.body.position.y>= 550 && waterballoon.body.position.y<=600){
-
-    console.log("rl");
-    lineNumber=1
-  }
 }
 
-function mouseReleased()
-{
-	slingshot.fly();
-  console.log("mouseReleased");
-  console.log(waterballoon.body.position.x);
-  console.log(waterballoon.body.position.y);
-
-if(lineNumber===4){
-  console.log("bl");
-  Score=Score+40;
-}
-
-if(lineNumber===3){
-  console.log("yl");
-  Score=Score+30;
-}
-
-if(lineNumber===2){
-  console.log("gl");
-  Score=Score+20;
-}
-
-if(lineNumber===1){
-  console.log("rl");
-  Score=Score+10;
-}
-}
-
-function keyPressed() {
-	if (keyCode === 32) {
-	  slingshot.attach(waterballoon.body);
-	}
-  }
